8d888c5790c66fbd0e58e3a3680506488fa0cbf2
AndyOximenes <andy.oximenes@gmail.com>
2020-06-24 17:46:04 -0300

    Add featured to the Starter courses card

--- data.js
+++ data.js
@@ -1,26 +1,27 @@
 module.exports = [
-  {
-    id: "starter",
-    image:
-      "https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
-    content: "Starter",
-    info: "4 Módulos",
-    price: "Free",
-  },
-  {
-    id: "launchbase",
-    image:
-      "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg",
-    content: "Launchbase",
-    info: "8 Módulos",
-    price: "PAGO",
-  },
-  {
-    id: "gostack",
-    image:
-      "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
-    content: "GoStack",
-    info: "14 Módulos",
-    price: "PAGO",
-  },
+    {
+        id: "starter",
+        image:
+            "https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
+        content: "Starter",
+        info: "4 Módulos",
+        price: "Free",
+        featured: true,
+    },
+    {
+        id: "launchbase",
+        image:
+            "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg",
+        content: "Launchbase",
+        info: "8 Módulos",
+        price: "PAGO",
+    },
+    {
+        id: "gostack",
+        image:
+            "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
+        content: "GoStack",
+        info: "14 Módulos",
+        price: "PAGO",
+    },
 ];
--- server.js
+++ server.js
@@ -9,54 +9,56 @@ server.use(express.static("public"));
 server.set("view engine", "njk");
 
 nunjucks.configure("views", {
-  express: server,
-  autoescape: false,
+    express: server,
+    autoescape: false,
+    noCache: true,
 });
 
 server.get("/", (req, res) => {
-  return res.render("courses", { items: courses });
+    return res.render("courses", { items: courses });
 });
 
 server.get("/about", (req, res) => {
-  const about = {
-    avatar_url:
-      "https://rocketseat.gallerycdn.vsassets.io/extensions/rocketseat/rocketseatreactnative/3.0.1/1588456740326/Microsoft.VisualStudio.Services.Icons.Default",
-    company: "Rocketseat",
-    description:
-      "As melhores tecnologias em programação, direto ao ponto e do jeito certo.",
-    main_techs: "Principais tecnologias da Rocketseat",
-    techs: [
-      { name: "React.JS" },
-      { name: "React Native" },
-      { name: "Node.JS" },
-    ],
-    links: [
-      {
-        name: "Site",
-        url: "https://rocketseat.com.br/",
-      },
-      {
-        name: "Github",
-        url: "https://github.com/Rocketseat",
-      },
-      {
-        name: "LinkedIn",
-        url: "https://www.linkedin.com/school/rocketseat/?originalSubdomain=br",
-      },
-      {
-        name: "Facebook",
-        url: "https://www.facebook.com/rocketseat/",
-      },
-    ],
-  };
+    const about = {
+        avatar_url:
+            "https://rocketseat.gallerycdn.vsassets.io/extensions/rocketseat/rocketseatreactnative/3.0.1/1588456740326/Microsoft.VisualStudio.Services.Icons.Default",
+        company: "Rocketseat",
+        description:
+            "As melhores tecnologias em programação, direto ao ponto e do jeito certo.",
+        main_techs: "Principais tecnologias da Rocketseat",
+        techs: [
+            { name: "React.JS" },
+            { name: "React Native" },
+            { name: "Node.JS" },
+        ],
+        links: [
+            {
+                name: "Site",
+                url: "https://rocketseat.com.br/",
+            },
+            {
+                name: "Github",
+                url: "https://github.com/Rocketseat",
+            },
+            {
+                name: "LinkedIn",
+                url:
+                    "https://www.linkedin.com/school/rocketseat/?originalSubdomain=br",
+            },
+            {
+                name: "Facebook",
+                url: "https://www.facebook.com/rocketseat/",
+            },
+        ],
+    };
 
-  return res.render("about", { about });
+    return res.render("about", { about });
 });
 
 server.use((req, res) => {
-  res.status(404).render("not-found");
+    res.status(404).render("not-found");
 });
 
 server.listen(5000, () => {
-  console.log("🚀 Server is running - port 5000");
+    console.log("🚀 Server is running - port 5000");
 });
